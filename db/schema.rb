7ec01cb9293c174946c5d410abd55d9458d16068
Mihail K <mihail@platterz.ca>
2017-01-28 14:41:57 -0500

    Rubocop.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20170128191746) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,5 +23,4 @@ ActiveRecord::Schema.define(version: 20170128191746) do
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["name"], name: "index_users_on_name", unique: true, using: :btree
   end
-
 end
